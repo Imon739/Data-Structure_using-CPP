c77b206fde572aa155fb9dde305b84f17857b8d0
Sazzad Hossain Imon <135940598+Imon739@users.noreply.github.com>
2024-07-02 22:04:06 +0600

    Update Bubble_sort.cpp

--- Bubble_sort.cpp
+++ Bubble_sort.cpp
@@ -3,27 +3,25 @@ using namespace std;
 int main()
 {
     int n;
+    cout << "Enter the size of the array: ";
     cin >> n;
+
     int arr[n];
-    for (int i=0;i<n;i++)
-    {
+    cout << "Enter the elements of the array: ";
+    for (int i = 0; i < n; i++) {
         cin >> arr[i];
     }
 
-    int temp;
-    for (int i=0;i<n-1;i++)
-    {
-        for (int j=0;j<n-1;j++)
-        {
-            if(arr[j] > arr[j+1])
-            {
-            temp = arr[j];
-            arr[j] = arr[j+1];
-            arr[j+1] = temp;
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < n - 1 - i; j++) {
+            if (arr[j] > arr[j + 1]) {
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
             }
         }
     }
-
+    cout << "After bubble sort : ";
     for (int i=0;i<n;i++)
     {
         cout << arr[i] << " ";
